2c589b10a8851d4d2bb98f38a34837d5e1ca0dd3
proteomics <meera.prasad@nyumc.org>
2016-11-17 14:25:25 -0500

    .

--- 007-msgfplus/match_peptides.py
+++ 007-msgfplus/match_peptides.py
@@ -1,19 +1,26 @@
 import os
 import pandas as pd
+import regex
 from pandas.core.common import intersection
 
 path = os.getcwd()
 input_path = path + "\\input\\"
 output_path = path + "\\output\\"
+result_out_path = '{0}msgfp_v1\\'.format(output_path)
 
 # msgfdb_tsv = '{0}KO_RPick_02_23_LTQ_2_24Feb06_Sphinx_06-01-07_msgfdb_fht.txt'.format(input_path)
 blib_tsv = '{0}Ralstonia_pickettii.blib.tsv'.format(input_path)
-merged_msgfplus_tsv = '{0}KO_Rpick_Merged.tsv'.format(output_path)
-result_file = '{0}matched_pepdtide_results.txt'.format(output_path)
+merged_msgfplus_tsv = '{0}KO_Rpick_Merged.tsv'.format(result_out_path)
+result_file = '{0}matched_pepdtide_results.txt'.format(result_out_path)
 
-blib_peptide_file = '{}Ralstonia_pickettii.blib.peptides.txt'.format(output_path)
-msgfplus_peptide_file = '{}KO_Rpick_Merged_MsgfPlus.peptides.txt'.format(output_path)
-matched_peptide_file = '{}blib_msgfplus_matched_peptides.txt'.format(output_path)
+blib_peptide_file = '{}Ralstonia_pickettii.blib.peptides.txt'.format(result_out_path)
+msgfplus_peptide_file = '{}KO_Rpick_Merged_MsgfPlus.peptides.txt'.format(result_out_path)
+msgfplus_peptide_filtered_file = '{}KO_Rpick_Merged_qfilter_0.0001.txt'.format(result_out_path)
+matched_peptide_file = '{}blib_msgfplus_matched_peptides.txt'.format(result_out_path)
+unmatched_peptide_file = '{}blib_msgfplus_unmatched_peptides.txt'.format(result_out_path)
+qfilter_matched_peptide_file = '{}blib_msgfplus_qfilter_matched_peptides.txt'.format(result_out_path)
+qfilter_unmatched_peptide_file = '{}blib_msgfplus_qfilter_unmatched_peptides.txt'.format(result_out_path)
+unmatched_overlaps_peptide_file = '{}msgfplus_unmatched_overlap_peptides.txt'.format(result_out_path)
 
 # df_msgfdb = pd.read_table(msgfdb_tsv)
 df_blib = pd.read_table(blib_tsv)
@@ -22,27 +29,62 @@ df_msgfplus = pd.read_table(merged_msgfplus_tsv)
 # list_peptide_msgfdb = df_msgfdb['Peptide'].tolist()
 list_peptide_blib = df_blib['peptideSeq'].tolist()
 list_peptide_msgfplus = df_msgfplus['Peptide'].tolist()
+list_peptide_msgfplus_qfilter = []
+
+with open(msgfplus_peptide_filtered_file) as file:
+    for line in file.read().splitlines():
+        list_peptide_msgfplus_qfilter.append(line)
+
 
 # set_msgfdb = set(list_peptide_msgfdb)
 set_blib = set(list_peptide_blib)
+set_msgfplus_fixed = set()
 set_msgfplus = set(list_peptide_msgfplus)
+set_msgfplus_qfilter = set(list_peptide_msgfplus_qfilter)
 
-set_matched = intersection(set_blib, set_msgfplus)
+for msgf_pep in set_msgfplus:
+    match = regex.search(r'\.{1}.+\.', msgf_pep)
+    if match:
+        pep_seq = match.group(0)
+        reg_pattern = regex.compile('[^a-zA-Z]')
+        pep_seq = reg_pattern.sub('', pep_seq)
+        set_msgfplus_fixed.add(pep_seq)
 
-with open(result_file, "a") as result:
+set_matched = intersection(set_blib, set_msgfplus_fixed)
+set_matched_qfilter = intersection(set_blib, set_msgfplus_qfilter)
+
+set_unmatched = set(set_blib).symmetric_difference(set_msgfplus_fixed)
+set_unmatched_qfilter = set(set_blib).symmetric_difference(set_msgfplus_qfilter)
+
+with open(result_file, "w") as result:
     # result.write(('all peptide count in msgfdb database : {0}\n'.format(list_peptide_msgfdb.__len__())))
     result.write(('all peptide count in blib database : {0}\n'.format(list_peptide_blib.__len__())))
     result.write(('all peptide count in msgfplus merged database : {0}\n'.format(list_peptide_msgfplus.__len__())))
     # result.write(('unique peptide count in msgfdb database : {0}\n'.format(set_msgfdb.__len__())))
     result.write(('unique peptide count in blib database : {0}\n'.format(set_blib.__len__())))
-    result.write(('unique peptide count in msgfplus merged database : {0}\n'.format(set_msgfplus.__len__())))
-    result.write(('unique peptide matched count between databases : {0}\n'.format(set_matched.__len__())))
+    result.write(('unique peptide count in msgfplus merged database : {0}\n'.format(set_msgfplus_fixed.__len__())))
+    result.write(('unique peptide count in msgfplus qfilter database : {0}\n'.format(set_msgfplus_qfilter.__len__())))
+    result.write(('unique peptide matched count for msgfplus merged-blib : {0}\n'.format(set_matched.__len__())))
+    result.write(('unique peptide matched count for msgfplus qfilter-blib : {0}\n'.format(set_matched_qfilter.__len__())))
 
-with open(blib_peptide_file, "a") as f:
+with open(blib_peptide_file, "w") as f:
     f.write("\n".join(set_blib))
 
-with open(msgfplus_peptide_file, "a") as f:
-    f.write("\n".join(set_msgfplus))
+with open(msgfplus_peptide_file, "w") as f:
+    f.write("\n".join(set_msgfplus_fixed))
 
-with open(matched_peptide_file, "a") as f:
+with open(matched_peptide_file, "w") as f:
     f.write("\n".join(set_matched))
+
+with open(qfilter_matched_peptide_file, "w") as f:
+    f.write("\n".join(set_matched_qfilter))
+
+with open(unmatched_peptide_file, "w") as f:
+    f.write("\n".join(set_unmatched))
+
+with open(qfilter_unmatched_peptide_file, "w") as f:
+    f.write("\n".join(set_unmatched_qfilter))
+
+set_unmatched_overlaps = intersection(set_unmatched, set_unmatched_qfilter)
+with open(unmatched_overlaps_peptide_file, "w") as f:
+    f.write("\n".join(set_unmatched_overlaps))
--- 007-msgfplus/msgfplus_mzml_mzid_tsv_conversion.py
+++ 007-msgfplus/msgfplus_mzml_mzid_tsv_conversion.py
@@ -5,28 +5,35 @@ import utility
 
 # input_path = 'E:\\Desktop\\test\\input'
 
-
-mzml_path = 'F:\\MSV000079053_MZML\\Ralstonia_pickettii'
 path = os.getcwd()
+input_path = path + "\\input\\"
 output_path = path + "\\output\\"
+java_exec_path = 'C:\\Program Files\\Java\\jre1.8.0_112\\bin\\java.exe'
+msgfplus_jar_path = 'D:\\Server\\MSGFPlus\\MSGFPlus.jar'
+mzml_path = 'F:\\MSV000079053_MZML\\Ralstonia_pickettii'
+msgfp_mods_file_path = '{0}MSGFPlus_Mods-20161117.txt'.format(input_path)
+db_fasta_file_path = '{0}ID_004956_793E1036.fasta'.format(input_path)
+out_result_folder = "{0}msgfp_v2".format(output_path)
 
-mzml_mzid_cmd = '"C:\\Program Files\\Java\\jre1.8.0_112\\bin\\java.exe" -Xmx4000M ' \
-                '-jar "D:\\Server\\MSGFPlus\\MSGFPlus.jar" ' \
-                '-s "F:\\MSV000079053_MZML\\Ralstonia_pickettii\\{0}.mzML" ' \
-                '-o "F:\\Proteomics\\007-msgfplus\\output\\{0}.mzid" ' \
-                '-d "F:\\Proteomics\\007-msgfplus\\input\\ID_004956_793E1036.fasta" ' \
+mzid_to_tsv_exec_path = 'D:\\Server\\MSGFPlus\\MzidToTsvConverter\\MzidToTsvConverter.exe'
+
+mzml_mzid_cmd = '"{java_exec_path}" -Xmx4000M ' \
+                '-jar "{msgfplus_jar_path}" ' \
+                '-s "{mzml_path}\\{filename}.mzML" ' \
+                '-o "{out_result_folder}\\{filename}.mzid" ' \
+                '-d "{db_fasta_file_path}" ' \
                 '-t 0.5Da -m 1 -inst 0 -e 1  -ntt 0 -tda 1 -minLength 6 -maxLength 40 -n 1 -thread 8 ' \
-                '-mod "F:\\Proteomics\\007-msgfplus\\input\\MSGFPlus_Mods.txt"'
+                '-mod "{msgfp_mods_file_path}"'
 
-mzid_tsv_cmd = 'D:\\Server\\MSGFPlus\\MzidToTsvConverter\\MzidToTsvConverter.exe ' \
-               '-mzid:"F:\\Proteomics\\007-msgfplus\\output\\{0}.mzid" ' \
-               '-tsv:"F:\\Proteomics\\007-msgfplus\\output\\{0}.tsv"'
+mzid_tsv_cmd = '{mzid_to_tsv_exec_path} ' \
+               '-mzid:"{input_folder}\\{filename}.mzid" ' \
+               '-tsv:"{output_folder}\\{filename}.tsv"'
 
-tsv_file_merged = '{0}KO_Rpick_Merged.tsv'.format(output_path)
+tsv_file_merged = '{0}\\KO_Rpick_Merged.tsv'.format(out_result_folder)
 list_tsv_files = []
 list_ignore = []
 
-for file in glob.glob(os.path.join(output_path, '*.mzid')):
+for file in glob.glob(os.path.join(out_result_folder, '*.mzid')):
     filenameext = os.path.basename(file)
     filename = os.path.splitext(filenameext)[0]
     list_ignore.append(filename)
@@ -35,14 +42,23 @@ for file in glob.glob(os.path.join(mzml_path, '*.mzML')):
     filenameext = os.path.basename(file)
     filename = os.path.splitext(filenameext)[0]
     if filename not in list_ignore:
-        subprocess.call(mzml_mzid_cmd.format(filename), shell=True)
+        subprocess.call(mzml_mzid_cmd.format(java_exec_path = java_exec_path,
+                                             msgfplus_jar_path = msgfplus_jar_path,
+                                             mzml_path = mzml_path,
+                                             db_fasta_file_path = db_fasta_file_path,
+                                             out_result_folder = out_result_folder,
+                                             msgfp_mods_file_path = msgfp_mods_file_path,
+                                             filename = filename), shell=True)
 
 for file in glob.glob(os.path.join(mzml_path, '*.mzML')):
     filenameext = os.path.basename(file)
     filename = os.path.splitext(filenameext)[0]
-    subprocess.call(mzid_tsv_cmd.format(filename), shell=True)
+    subprocess.call(mzid_tsv_cmd.format(mzid_to_tsv_exec_path = mzid_to_tsv_exec_path,
+                                        input_folder = out_result_folder,
+                                        output_folder = out_result_folder,
+                                        filename = filename), shell=True)
 
-for file in glob.glob(os.path.join(output_path, '*.tsv')):
+for file in glob.glob(os.path.join(out_result_folder, '*.tsv')):
     list_tsv_files.append(file)
 
 utility.concat_files(list_tsv_files, tsv_file_merged, True)
